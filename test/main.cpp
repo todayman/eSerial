f074fcbd55d38c685e9da4796df0c76a25b0891f
Paul O'Neil <redballoon36@gmail.com>
2012-01-03 12:21:47 -0500

    Proper-constness

--- test/main.cpp
+++ test/main.cpp
@@ -6,11 +6,6 @@
 //  Copyright (c) 2011 Paul O'Neil. All rights reserved.
 //
 
-/*
- Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.
- TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=
-*/
-//#include <libxml/tree.h>
 #include <iostream>
 using namespace std;
 #include "Writable.h"
@@ -35,7 +30,7 @@ public:
     writer->write("A message in a bottle!", "msg");
   }
   
-  virtual void read(Parser * parser) const override {
+  virtual void read(Parser * parser) override {
   }
 };
 
@@ -48,7 +43,7 @@ public:
     writer->write(c, "car");
   }
   
-  virtual void read(Parser * reader) const override {
+  virtual void read(Parser * reader) override {
   }
 };
 
