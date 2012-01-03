7a0fe17551cbd8df85df03d2c33358faeeec1b1b
Paul O'Neil <redballoon36@gmail.com>
2012-01-03 12:18:56 -0500

    Indent using Tabs, not spaces, in Writer.h

--- eSerial/Writer.h
+++ eSerial/Writer.h
@@ -29,62 +29,63 @@ public:
 			return message;
 		}
 	};
-
+	
 protected:
 	std::map<const Writable*, Object*> idList;
 	std::vector<Object*> root_objs;
 	Object * curObj;
-  Object * newObject(const Writable * object);
-  Object * addObjectGetID(const Writable * object);
-  
-  template<typename T>
-  void write_impl(T val, const std::string& name);
-  
-  /* these structs are here to route
-   instantiations of write() to the correct implementation */
-  template<bool primitve, typename T>
-  struct is_base_type_thingy {
-  };
-  
-  template<typename T>
-  struct is_base_type_thingy<false, T> {
-    static_assert(std::is_base_of<Writable, T>::value,
-                  "You must inherit from eos::serializable::Writable to be write an object.");
-    typedef const Writable& theType;
-  };
-  
-  template<typename T>
-  struct is_base_type_thingy<true, T> {
-    typedef T theType;
-  };
-  
+	
+	Object * newObject(const Writable * object);
+	Object * addObjectGetID(const Writable * object);
+	
+	template<typename T>
+	void write_impl(T val, const std::string& name);
+	
+	/* these structs are here to route
+	 instantiations of write() to the correct implementation */
+	template<bool primitve, typename T>
+	struct is_base_type_thingy {
+	};
+	
+	template<typename T>
+	struct is_base_type_thingy<false, T> {
+		static_assert(std::is_base_of<Writable, T>::value,
+									"You must inherit from eos::serializable::Writable to be write an object.");
+		typedef const Writable& theType;
+	};
+	
+	template<typename T>
+	struct is_base_type_thingy<true, T> {
+		typedef T theType;
+	};
+	
 public:
-  Writer() : idList(), root_objs(), curObj(nullptr) { }
-  virtual ~Writer();
+	Writer() : idList(), root_objs(), curObj(nullptr) { }
+	virtual ~Writer();
 	virtual void writeFile(const std::string& pathname) = 0;
 	void addObject(Writable * object) {
-    addObjectGetID(object);
-  }
-  void writeName(const std::string& name) throw(NoCurrentObject);
-  
+		addObjectGetID(object);
+	}
+	void writeName(const std::string& name) throw(NoCurrentObject);
+	
 	/* write individual values */
-  template<typename T>
-  void write(T val, const std::string& name) {
-    static_assert( !std::is_pointer<T>::value || std::is_base_of<Writable, T>::value,
-                  "Only pointers to subclasses of eos::serialization::Writable may be written.");
-    write_impl< typename is_base_type_thingy<std::is_scalar<T>::value, T>::theType >(val, name);
-  }
-  
+	template<typename T>
+	void write(T val, const std::string& name) {
+		static_assert( !std::is_pointer<T>::value || std::is_base_of<Writable, T>::value,
+									"Only pointers to subclasses of eos::serialization::Writable may be written.");
+		write_impl< typename is_base_type_thingy<std::is_scalar<T>::value, T>::theType >(val, name);
+	}
+	
 	/* write arrays of values */
-  template<typename T>
-  void writeArray(T * elements, size_t count, const std::string& name, hint_t hint = NO_HINT) throw(NoCurrentObject);
-  
-  // Get a writer
-  static Writer * newXMLWriter();
+	template<typename T>
+	void writeArray(T * elements, size_t count, const std::string& name, hint_t hint = NO_HINT) throw(NoCurrentObject);
+	
+	// Get a writer
+	static Writer * newXMLWriter();
 };
 
-  template<> inline void Writer::write<const char*>(const char * val, const std::string& name) {
-  write_impl<const char *>(val, name);
+template<> inline void Writer::write<const char*>(const char * val, const std::string& name) {
+	write_impl<const char *>(val, name);
 }
 
 } // namespace serialization
