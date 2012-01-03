eabb19ef1e2e1f35d79da516f40e6e28b44b2742
Paul O'Neil <redballoon36@gmail.com>
2012-01-03 01:38:27 -0500

    Use GoogleTest instead of CUnit

--- test/b64_tests.cpp
+++ test/b64_tests.cpp
@@ -5,44 +5,26 @@
 //  Created by Paul O'Neil on 12/22/11.
 //  Copyright (c) 2011 Paul O'Neil. All rights reserved.
 //
-#include <stdlib.h>
-#include <stdio.h>
-#include <CUnit/CUnit.h>
+
+#include <gtest/gtest.h>
 
 #include "b64.h"
-#include "unittests.h"
 
 static const char text[] = "Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.";
 static const char data[] = "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=";
 
-static void test_encode(void) {
+TEST(Base64Test, Base64EncodingTest) {
 	char * encoded;
 	// length of the array includes the null terminator, which isn't part of the data
 	convert_to_base64(text, sizeof(text) - 1, &encoded);
-	CU_ASSERT_STRING_EQUAL(encoded, data);
+	EXPECT_EQ(memcmp(encoded, data, sizeof(data)-1), 0);
 	free(encoded);
 }
 
-static void test_decode(void) {
+TEST(Base64Test, Base64DecodingTest) {
 	char * decoded;
 	// length of the array includes the null terminator, which isn't part of the data
 	convert_from_base64(data, sizeof(data) - 1, &decoded);
-	CU_ASSERT_STRING_EQUAL(decoded, text);
+	EXPECT_EQ(memcmp(decoded, text, sizeof(text) - 1), 0);
 	free(decoded);
-}
-
-int add_b64_tests(void) {
-  CU_pSuite suite = CU_add_suite("Base64 Encoding Suite", NULL, NULL);
-  if( NULL == suite ) {
-    return -1;
-  }
-  
-  if( (NULL == CU_add_test(suite, "To Base64", test_encode)) ||
-		 (NULL == CU_add_test(suite, "From Base64", test_decode))
-     )
-  {
-    return -1;
-  }
-  
-  return 0;
 }
--- test/unittests.cpp
+++ test/unittests.cpp
@@ -6,24 +6,10 @@
 //  Copyright (c) 2011 Paul O'Neil. All rights reserved.
 //
 
-#include <CUnit/CUnit.h>
-#include <CUnit/Basic.h>
+#include <gtest/gtest.h>
 
-#include "unittests.h"
-
-int main()
+int main(int argc, char ** argv)
 {
-  if( CUE_SUCCESS != CU_initialize_registry() ) {
-    return CU_get_error();
-  }
-  
-  if( add_b64_tests() ) {
-    CU_cleanup_registry();
-    return CU_get_error();
-  }
-  
-  CU_basic_set_mode(CU_BRM_VERBOSE);
-  CU_basic_run_tests();
-  CU_cleanup_registry();
-  return CU_get_error();
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
