11ab740b24ed7755a546968f339f567a1e5a6d3e
Paul O'Neil <redballoon36@gmail.com>
2012-01-06 22:41:09 -0500

    Documented the array hints

--- eSerial/hints.h
+++ eSerial/hints.h
@@ -14,9 +14,13 @@ namespace serialization {
 typedef uint32_t hint_t;
 
 constexpr hint_t NO_HINT        = 0;
+//! Hints that the data should be stored in a human-readable format
 constexpr hint_t READABLE_HINT  = 1 << 0;
+//! Hints that the data should be stored in a binary format to conserve space
 constexpr hint_t BINARY_HINT    = READABLE_HINT << 1;
+//! Hints that the array should be copied, not referenced, before serialization
 constexpr hint_t COPY_ARRAY_HINT= BINARY_HINT << 1;
+//! Hints that the data should be freed with free(), not delete[]
 constexpr hint_t FREE_HINT      = COPY_ARRAY_HINT << 1;
 
 } // namespace serialization
