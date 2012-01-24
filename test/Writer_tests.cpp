841520a8568b975b5e1a4de408f645aa89027863
Paul O'Neil <redballoon36@gmail.com>
2012-01-23 20:25:08 -0500

    GPL

--- test/Writer_tests.cpp
+++ test/Writer_tests.cpp
@@ -1,9 +1,22 @@
 //
 //  eos/serialization/Writer_tests.cpp
-//  eSerial
 //
-//  Created by Paul O'Neil on 12/24/11.
-//  Copyright (c) 2012 Paul O'Neil. All rights reserved.
+//  Copyright 2012 Paul O'Neil.
+//
+//  This file is part of eSerial.
+//
+//  eSerial is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  eSerial is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with eSerial.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #include <eSerial/Writer.h>
